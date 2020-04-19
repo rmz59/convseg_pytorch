f84def3429a84a0577f513c47b0a816cdd1d2961
Runmin Zhang <z.runmin@gmail.com>
2020-04-18 23:34:41 -0400

    update preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,55 @@
 from typing import List
 
 
+# -----------------------------------------------------------------------------
+#               Process vocabulary
+# -----------------------------------------------------------------------------
+def build_vocab(items, add_unk=True, add_pad=True):
+    """ build vocabularies from a item list
+    Args:
+        items: list of items
+        add_unk: bool. Whether to add unk 
+    
+    >>> build_vocab(["a","b","b"], add_unk=False, add_pad=True)
+    {'a': 1, 'b': 2, '<PAD>': 1e+20}
+    """
+    assert type(items) in (list, tuple)
+    dic = {}
+    for item in items:
+        dic[item] = dic.get(item, 0) + 1
+    if add_pad:
+        dic['<PAD>'] = 1e20
+    if add_unk:
+        dic['<UNK>'] = 1e10
+    return dic
+
+def add_id(items):
+    """ add ids to tokents
+
+    Args:
+        items: dict or list
+    Returns:
+        token2id, id2token: dict, dict
+    
+    >>> add_id({'a':10,'b':2,'<UNK>':1e20})
+    ({'<UNK>': 0, 'a': 1, 'b': 2}, {0: '<UNK>', 1: 'a', 2: 'b'})
+    >>> add_id(["S","D","E","S"])
+    ({'S': 3, 'D': 1, 'E': 2}, {0: 'S', 1: 'D', 2: 'E', 3: 'S'})
+    """
+    if type(items) is dict:
+        sorted_items = sorted(items.items(), key=lambda x: (-x[1], x[0]))
+        id2token = {i: v[0] for i, v in enumerate(sorted_items)}
+        token2id = {v: k for k, v in id2token.items()}
+        return token2id, id2token
+    elif type(items) is list:
+        id2token = {i: v for i, v in enumerate(items)}
+        token2id = {v: k for k, v in id2token.items()}
+        return token2id, id2token
+
+
+# -----------------------------------------------------------------------------
+#               Process Sentence
+# -----------------------------------------------------------------------------
 def process_sentence(sentence: str, stage='train'):
     """ process a single sentence
 
