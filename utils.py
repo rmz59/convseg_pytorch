480c9c8404667a30eccfd855d2dd6085e7e1fb43
Runmin Zhang <z.runmin@gmail.com>
2020-04-19 13:57:33 -0400

    add pad_sents

--- utils.py
+++ utils.py
@@ -133,3 +133,20 @@ def get_feature(sentences: List[str], stage='train'):
             tags_array.append(tags)
     return chars_array, tags_array
 
+
+def pad_sents(sents, pad_token):
+    """ Pad list of sentences according to the longest sentence in the batch.
+        The paddings should be at the end of each sentence.
+    @param sents (list[list[str]]): list of sentences, where each sentence
+                                    is represented as a list of words
+    @param pad_token (str): padding token
+    @returns sents_padded (list[list[str]]): list of sentences where sentences shorter
+        than the max length sentence are padded out with the pad_token, such that
+        each sentences in the batch now has equal length.
+    """
+    sents_padded = []
+
+    max_len = max(len(s) for s in sents)
+    sents_padded = [s + [pad_token]*(max_len - len(s)) for s in sents] 
+
+    return sents_padded
